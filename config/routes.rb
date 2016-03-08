0e7f1fc83e8c3a261616e901ead3fe78bed51f5a
tattyamm <entatsu.appli+github@gmail.com>
2016-03-08 16:56:19 +0000

    add api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :trends
+  resources :trends #debug用
+
+  get '/api/trend/google', to: 'api#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
