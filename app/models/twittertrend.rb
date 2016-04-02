e21e8017155434279a744812338853d25b033c4c
tattyamm <entatsu.appli+github@gmail.com>
2016-04-02 16:54:47 +0000

    add twitter trend

--- app/models/twittertrend.rb
+++ app/models/twittertrend.rb
@@ -1,13 +1,43 @@
+# promoted_content というパラメタがある
+
 class Twittertrend
   include ActiveModel::Model
 
   def self.get
-    puts ENV["consumerKey"]
+    trendList = Array.new
+    getTrend.each { |item|
+      eachItem = {
+          "title" => item.name,
+          "link" => "https://twitter.com/search?" + item.url.query,
+          "pubDate" => DateTime.now.strftime("%Y-%m-%d"),
+          "description" => "",
+      }
+      trendList.push(eachItem)
+    }
+    trendList
 
-    output = []
+
+    output = {
+        "value" => {
+            "title" => "twitter trend",
+            "link" => "https://twitter.com/",
+            "description" => "",
+            "item" => trendList
+        }
+    }
     output
   end
 
+  def self.getTrend
+    twClient = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["consumerKey"]
+      config.consumer_secret     = ENV["consumerSecret"]
+      #  tokenを渡さないでApplication-only authenticationにする
+      #  config.access_token        = TW_ACCESS_TOKEN
+      #  config.access_token_secret = TW_ACCESS_TOKEN_SECRET
+    end
+    twClient.trends(23424856)
 
+  end
 
 end
