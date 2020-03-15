1f61e147528c890647be3858a971f16027f8f7e0
tattyamm <entatsu.appli+github@gmail.com>
2020-03-15 05:47:11 +0000

    remove raise_in_transactional_callbacks setting

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Checktrend
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
 
     # redis
     config.cache_store = :redis_store, ENV["REDISTOGO_URL"] + "0/cache", { expires_in: 2.minutes }
